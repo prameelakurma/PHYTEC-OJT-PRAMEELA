83f7c2d18cd31558c87578d2b42433ef7b621e0a
prameela <124024254+prameelakurma@users.noreply.github.com>
2024-02-18 11:14:34 +0530

    Add files via upload

---
+++ 08_Static_Dynamic_Lib/1_static_lib/static_prog/temp/userprog.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main()
+{
+	print("Learning static Library usage\n", 5);
+	return 0;
+
+}
---
+++ 08_Static_Dynamic_Lib/2_dynamic_lib/dynamic_prog/temp/userprog.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main()
+{
+	print("Learning Dynamic Library Usage\n", 5);
+	return 0;
+}
